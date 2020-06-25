e7f3b3140a349a7253766123dc8a179ea6576073
pradeepradyumna <pradeepradyumna@gmail.com>
2020-06-25 21:29:15 +0530

    used pipe to pass from scan to subscribe

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
-document.addEventListener('click',()=>{
-  console.log("clicked");
-})
+import { fromEvent } from "rxjs";
+import { scan } from "rxjs/operators";
+
+fromEvent(document, "click")
+  .pipe(scan(count => count + 1, 0))
+  .subscribe(count => console.log("Clicked" + count));
