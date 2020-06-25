4b7a738d317bc8fdf71bac2de7c1f51da1072c1e
pradeepradyumna <pradeepradyumna@gmail.com>
2020-06-25 21:24:24 +0530

    basic JS to understand how click works

--- index.ts
+++ index.ts
@@ -1,6 +1,3 @@
-import { from } from "rxjs";
-
-const observable = from([10, 20, 40, 50]);
-
-const subscription = observable.subscribe(x => console.log(x));
-subscription.unsubscribe();
+document.addEventListener('click',()=>{
+  console.log("clicked");
+})
